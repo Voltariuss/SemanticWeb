d637fbc95d72a3affb3741ea43b41182749bf2b0
Sekanor <sekanor21@gmail.com>
2021-01-13 16:16:35 +0100

    Début d'IHM

--- criminal.js
+++ criminal.js
@@ -1,6 +1,6 @@
-const criminal = 'http://dbpedia.org/resource/Luciano_Leggio'
+const criminalURI = 'http://dbpedia.org/resource/John_Edward_Robinson'
 
-const name = [
+const fields = [
     'name',
     'alias',
     'criminalCharge',
@@ -25,7 +25,7 @@ const predicates = [
     'dbo:criminalCharge',
     'dbo:convictionPenalty',
     'dbo:motive',
-    'dbpedia2:apprehended',
+    'dbo:apprehended',
     'dbpedia2:victims',
     'dbo:country',
     'dbo:birthDate',
@@ -38,19 +38,47 @@ const predicates = [
     'dbpedia2:imageName'
 ]
 
-const promises = predicates.map((p) => { return generateRequest(criminal, p) })
-promises.push(
-
-);
-
-
+const promises = predicates.map((p) => { return generateRequest(criminalURI, p) })
 Promise.all(promises)
     .then(results => {
         // build dom
         buildDOM(results);
     })
 
+function buildDOM(data) {
+    console.log('buildDom');
+    const criminal = {};
 
-function buildDOM(results) {
-    console.log(results)
+    for (let i = 0; i < fields.length; ++i) {
+        criminal[fields[i]] = []
+        const bindings = data[i]['results']['bindings'];
+
+        // Si l'API a retourné une ligne
+        if (bindings.length > 0) {
+            for (let j = 0; j < bindings.length; ++j) {
+                criminal[fields[i]].push(bindings[j]['value']);
+            }
+
+            // Suppression des lignes non anglaises
+            criminal[fields[i]] = criminal[fields[i]].filter(item => {
+                return item['xml:lang'] === undefined || item['xml:lang'] == 'en';
+            })
+
+            // Si l'API a retourné une ligne non anglaise
+            if (criminal[fields[i]][0] != undefined) {
+                $('#'+fields[i]).html(criminal[fields[i]][0]['value']);
+            }
+        }
+    }
+
+
+    requestSameYear('dbo:apprehended', criminal.apprehended[0]['value'], criminalURI).done((data2) => {
+        console.log(data2);
+
+        // $('#apprehended_same_year')
+    })
+
+    console.log(criminal)
+
+    
 }
