8c7c684883dc74b8e51abd1886b91502fedd781f
cyrelouyea <alerycserrania@protonmail.com>
2021-01-17 23:50:32 +0100

    Add criminal image

--- criminal.js
+++ criminal.js
@@ -1,8 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const criminalId = urlParams.get('id');
-const criminalURI = 'http://dbpedia.org/resource/'+criminalId;
-
-
+const criminalURI = 'http://dbpedia.org/resource/' + criminalId;
 
 const fields = [
     'name',
@@ -44,12 +42,16 @@ const predicates = [
     'dbpedia2:imageName'
 ]
 
+$('#criminal_info').hide();
 const promises = predicates.map((p) => { return generateRequest(criminalURI, p) })
 Promise.all(promises)
     .then(results => {
+        $('#criminal_info').show();
+        $('#criminal_info_loading').hide();
         // build dom
         buildDOM(results);
     })
+
 
 function getResourceName(resource, def) {
     if (resource['label']) {
@@ -71,10 +73,12 @@ function showSameApprehendedYear(criminal) {
         let str = '<ul>';
         for (let c of other['results']['bindings']) {
             const target = getResourceIdFromUri(c['criminal']['value']);
-            str += '<li><a href="./criminal.html?id='+target+'">' + getResourceName(c, 'criminal') + '</a></li>';
+            str += '<li><a href="./criminal.html?id=' + target + '">' + getResourceName(c, 'criminal') + '</a></li>';
         }
         str += '</ul>';
         $('#same_apprehended_year_list').append(str);
+        $('#same_apprehended_year').show();
+        $('#same_apprehended_year_loading').hide();
     });
 }
 
@@ -84,60 +88,65 @@ function showSameConvictionPenalty(criminal) {
         let str = '<ul>';
         for (let c of other['results']['bindings']) {
             const target = getResourceIdFromUri(c['criminal']['value']);
-            str += '<li><a href="./criminal.html?id='+target+'">' + getResourceName(c, 'criminal') + '</a></li>';
+            str += '<li><a href="./criminal.html?id=' + target + '">' + getResourceName(c, 'criminal') + '</a></li>';
         }
         str += '</ul>';
         $('#same_conviction_penalty_list').append(str);
+        $('#same_conviction_penalty').show();
+        $('#same_conviction_penalty_loading').hide();
     });
 }
 
 function showSimilarCriminalCharge(criminal) {
     let str = '<ul>';
 
-const promises = criminal.criminalCharge[0]['value'].split(/[;.,()]/)
+    const promises = criminal.criminalCharge[0]['value'].split(/[;.,()]/)
         .map(s => s.trim())
-        .filter(s => s.length > 0) 
+        .filter(s => s.length > 0)
         .map(piece => {
             console.log(piece)
             return requestContainsText('dbo:criminalCharge', piece.toLowerCase(), criminalURI)
-    });
+        });
 
     Promise.all(promises).then(pieces => {
-        
+
         for (let piece of pieces) {
             for (let c of piece['results']['bindings']) {
                 const target = getResourceIdFromUri(c['criminal']['value']);
-                str += '<li><a href="./criminal.html?id='+target+'">' + getResourceName(c, 'criminal') + '</a> (' + c['value']['value'] + ') </li>';
+                str += '<li><a href="./criminal.html?id=' + target + '">' + getResourceName(c, 'criminal') + '</a> (' + c['value']['value'] + ') </li>';
             }
         }
         str += '</ul>';
         $('#similar_criminal_charge_list').append(str);
+        $('#similar_criminal_charge').show();
+        $('#similar_criminal_charge_loading').hide();
     })
-    
+
 }
 
 function showSimilarMotive(criminal) {
     let str = '<ul>';
 
-    const promises = criminal.criminalCharge[0]['value'].split(/[;.,()]/)
+    const promises = criminal.motive[0]['value'].split(/[;.,()]/)
         .map(s => s.trim())
-        .filter(s => s.length > 0) 
+        .filter(s => s.length > 0)
         .map(piece => {
             return requestContainsText('dbo:motive', piece.toLowerCase(), criminalURI)
-    });
+        });
 
     Promise.all(promises).then(pieces => {
-        
         for (let piece of pieces) {
             for (let c of piece['results']['bindings']) {
                 const target = getResourceIdFromUri(c['criminal']['value']);
-                str += '<li><a href="./criminal.html?id='+target+'">' + getResourceName(c, 'criminal') + '</a> (' + c['value']['value'] + ') </li>';
+                str += '<li><a href="./criminal.html?id=' + target + '">' + getResourceName(c, 'criminal') + '</a> (' + c['value']['value'] + ') </li>';
             }
         }
         str += '</ul>';
-        $('#similar_criminal_charge_list').append(str);
-    })
-    
+        $('#similar_motive_list').append(str);
+        $('#similar_motive').show();
+        $('#similar_motive_loading').hide();
+    });
+
 }
 
 function buildDOM(data) {
@@ -157,11 +166,16 @@ function buildDOM(data) {
             // Suppression des lignes non anglaises
             criminal[fields[i]] = criminal[fields[i]].filter(item => {
                 return item['xml:lang'] === undefined || item['xml:lang'] == 'en';
-            })
+            });
 
-            // Si l'API a retourné une ligne non anglaise
+            // Si l'API a retourné une ligne anglaise
             if (criminal[fields[i]][0] != undefined) {
-                $('#'+fields[i]).html(criminal[fields[i]][0]['value']);
+                if (fields[i] == 'imageName') {
+                    const imgName = criminal[fields[i]][0]['value']
+                    $('#' + fields[i]).html(`<img src="https://wikipedia.org/wiki/Special:FilePath/${imgName}" title="${imgName}" alt="${imgName}" />`)
+                } else {
+                    $('#' + fields[i]).html(criminal[fields[i]][0]['value']);
+                }
             }
         }
     }
@@ -185,6 +199,13 @@ function buildDOM(data) {
         showSimilarCriminalCharge(criminal);
     } else {
         $('#similar_criminal_charge').hide();
+    }
+
+    // Show criminals with similar motive
+    if (criminal.motive.length > 0) {
+        showSimilarMotive(criminal);
+    } else {
+        $('#similar_motive').hide();
     }
 
     console.log(criminal);
