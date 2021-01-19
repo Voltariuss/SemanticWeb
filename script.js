6a9f1ffa45878395b81f34201b1eee21e11ed84a
nbouvier <nbouvier@gmail.com>
2021-01-19 11:51:59 +0100

    Mes couilles au bord de l'eau numéro Dos

--- script.js
+++ script.js
@@ -6,51 +6,99 @@ const prefixes = {
 };
 
 const filters = {
-  formName: 'FILTER(contains(lcase(str(?n)), lcase(str("%"))))',
-  formAlias: 'FILTER(contains(lcase(str(?a)), lcase(str("%"))))',
-  formCriminalCharge: 'FILTER(contains(lcase(str(?cc)), lcase(str("%"))))',
-  formConvictionPenalty: 'FILTER(contains(lcase(str(?cp)), lcase(str("%"))))',
-  formMotive: 'FILTER(contains(lcase(str(?m)), lcase(str("%"))))',
-  formApprenhendedBegin: 'FILTER(?d >= "%d"^^xsd:date)', // 19390101
-  formApprenhendedEnd: 'FILTER(?d <= "%d"^^xsd:date)',
-  formVictimsMin: 'FILTER(xsd:integer(?v) >= %)',
-  formVictimsMax: 'FILTER(xsd:integer(?v) <= %)',
-  formCountry: 'FILTER(?cnt = <http://dbpedia.org/resource/%>)'
+  formName: { filter: 'FILTER(contains(lcase(str(?n)), lcase(str("%"))))' },
+  formAlias: { filter: 'FILTER(contains(lcase(str(?a)), lcase(str("%"))))' },
+  formCriminalCharge: { variable: 'dbo:criminalCharge ?cc', filter: 'FILTER(contains(lcase(str(?cc)), lcase(str("%"))))' },
+  formConvictionPenalty: { variable: 'dbo:convictionPenalty ?cp', filter: 'FILTER(contains(lcase(str(?cp)), lcase(str("%"))))' },
+  formMotive: { variable: 'dbo:motive ?m', filter: 'FILTER(contains(lcase(str(?m)), lcase(str("%"))))' },
+  formApprenhendedBegin: { variable: 'dbpedia2:apprehended ?d', filter: 'FILTER(?d >= "%d"^^xsd:date)' },
+  formApprenhendedEnd: { variable: 'dbpedia2:apprehended ?d', filter: 'FILTER(?d <= "%d"^^xsd:date)' },
+  formVictimsMin: { variable: 'dbpedia2:victims ?v', filter: 'FILTER(xsd:integer(?v) >= %)' },
+  formVictimsMax: { variable: 'dbpedia2:victims ?v', filter: 'FILTER(xsd:integer(?v) <= %)' },
+  formCountry: { variable: 'dbo:country ?cnt', filter: 'FILTER(?cnt = <%>)' }
 }
 
-function onSubmitFilters() {
+function getCountries() {
 
-  let mdx = `${ Object.entries(prefixes).map(([alias, url]) => `PREFIX ${ alias } ${ url }`).join(' ') }
+  let mdx = `${ addPrefixes() }
+
+    SELECT DISTINCT ?cnt, ?cn WHERE {
+      ?c a criminal: ; foaf:name ?n ; dbpedia2:alias ?a ; rdfs:comment ?com  ; dbo:country ?cnt . ?cnt dbp:commonName ?cn
+    }
+  `;
+
+  dbPediaRequest(mdx, data => {
+
+    let json = data.results.bindings;
+
+    $('#formCountry').append(`${
+      Object.values(json).map(country =>
+        `<option value="${ country.cnt.value }">${ country.cn.value.split('/').pop() }</option>`
+      ).join('')
+    }`);
+
+  });
+
+}
+
+function onSubmitFilters(e) {
+
+  let mdx = `${ addPrefixes() }
+
     SELECT ?n, ?a, ?com WHERE {
-      ?c a criminal: ; foaf:name ?n ; dbpedia2:alias ?a ; rdfs:comment ?com .
-      OPTIONAL {
-        ?c dbo:criminalCharge ?cc ;
-        dbo:convictionPenalty ?cp ;
-        dbo:motive ?m ;
-        dbpedia2:apprehended ?d ;
-        dbpedia2:victims ?v ;
-        dbo:country ?cnt
-      }
-      ${ Object.entries(filters).map(createFilter).join(' ') }
+      ?c a criminal: ; foaf:name ?n ; dbpedia2:alias ?a ; rdfs:comment ?com ${ addVariables() }
+      ${ addFilters() }
     } LIMIT 100
   `;
+
+  e.preventDefault();
 
   dbPediaRequest(mdx, data => console.log(data));
 
 }
 
-function createFilter([inputName, filter]) {
+function addPrefixes() {
 
-  let inputValue = $(`#${ inputName }`).val();
+  return Object.entries(prefixes).map(([alias, url]) => `PREFIX ${ alias } ${ url }`).join(' ');
 
-  if(inputValue) {
-    filter = filter.replace('%d', $.format.date(inputValue, 'yyyyMMdd'));
+}
+
+function addVariables() {
+
+  let usedVariables = [];
+  let variables = Object.entries(filters).map(([inputName, filterOptions]) => {
+
+    let variable = filterOptions['variable'];
+    let inputValue = $(`#${ inputName }`).val();
+
+    if(!variable || !inputValue || usedVariables.includes(variable))
+    { return; }
+
+    usedVariables.push(variable);
+
+    return variable;
+
+  }).filter(value => value).join(' ; ');
+
+  return variables ? ` ; ${ variables }` : '';
+
+}
+
+function addFilters() {
+
+  return Object.entries(filters).map(([inputName, filterOptions]) => {
+
+    let filter = filterOptions.filter;
+    let inputValue = $(`#${ inputName }`).val();
+
+    if(!inputValue) { return; }
+
+    filter = filter.replace('%d', $.format.date(inputValue, 'yyyy-MM-dd'));
     filter = filter.replace('%', inputValue);
 
     return filter;
-  }
 
-  return '';
+  }).join(' ');
 
 }
 
@@ -63,4 +111,10 @@ function dbPediaRequest(sparqlRequest, callback) {
 
 }
 
-$('#submit').click(onSubmitFilters);
+$(document).ready(() => {
+
+  $('#submit').click(onSubmitFilters);
+
+  getCountries();
+
+});
